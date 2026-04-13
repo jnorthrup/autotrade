d2d4ec79ea4ff08d77e3521ac1ef05a3ffe71394
jnorthrup <jnorthrup@example.com>
2026-04-13 11:57:56 -0500

    pre-prune

---
+++ fetchrecurse.sh
@@ -0,0 +1,10 @@
+git  clone --recurse-submodules --shallow-submodules   --branch wann2  git@gitlab.com:jnorthrup1/mp         mp
+git  clone --recurse-submodules --shallow-submodules  --branch WANN   git@gitlab.com:fjLWN2z5O1/hrm.git     hrm
+git  clone --recurse-submodules --shallow-submodules    git@github.com:microjasa/binance-java-api.git       binance
+git  clone --recurse-submodules --shallow-submodules    git@gitlab.com:jnorthrup1/columnar.git              columnar
+
+
+git submodule add   --force   git@gitlab.com:jnorthrup1/mp                        mp
+git submodule add   --force   git@gitlab.com:fjLWN2z5O1/hrm.git                   hrm
+git submodule add   --force   git@github.com:microjasa/binance-java-api.git       binance
+git submodule add   --force   git@gitlab.com:jnorthrup1/columnar.git              columnar
